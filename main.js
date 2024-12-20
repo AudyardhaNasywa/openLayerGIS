2ae91551bab9b4402fc7a2a2192e486f0a5ed806
AudyardhaNasywa <audyardhaandini@gmail.com>
2024-12-20 10:29:37 +0700

    Open Layer GIS

---
+++ main.js
@@ -0,0 +1,82 @@
+import ImageTile from 'ol/source/ImageTile.js';
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import {
+  DragAndDrop,
+  defaults as defaultInteractions,
+} from 'ol/interaction.js';
+import {GPX, GeoJSON, IGC, KML, TopoJSON} from 'ol/format.js';
+import {
+  Tile as TileLayer,
+  VectorImage as VectorImageLayer,
+} from 'ol/layer.js';
+import {Vector as VectorSource} from 'ol/source.js';
+
+const dragAndDropInteraction = new DragAndDrop({
+  formatConstructors: [GPX, GeoJSON, IGC, KML, TopoJSON],
+});
+
+const key = 'Get your own API key at https://www.maptiler.com/cloud/';
+const attributions =
+  '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
+  '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
+
+const map = new Map({
+  interactions: defaultInteractions().extend([dragAndDropInteraction]),
+  layers: [
+    new TileLayer({
+      source: new ImageTile({
+        attributions: attributions,
+        url:
+          'https://api.maptiler.com/maps/satellite/{z}/{x}/{y}.jpg?key=' + key,
+        tileSize: 512,
+        maxZoom: 20,
+      }),
+    }),
+  ],
+  target: 'map',
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
+
+dragAndDropInteraction.on('addfeatures', function (event) {
+  const vectorSource = new VectorSource({
+    features: event.features,
+  });
+  map.addLayer(
+    new VectorImageLayer({
+      source: vectorSource,
+    }),
+  );
+  map.getView().fit(vectorSource.getExtent());
+});
+
+const displayFeatureInfo = function (pixel) {
+  const features = [];
+  map.forEachFeatureAtPixel(pixel, function (feature) {
+    features.push(feature);
+  });
+  if (features.length > 0) {
+    const info = [];
+    let i, ii;
+    for (i = 0, ii = features.length; i < ii; ++i) {
+      info.push(features[i].get('name'));
+    }
+    document.getElementById('info').innerHTML = info.join(', ') || '&nbsp';
+  } else {
+    document.getElementById('info').innerHTML = '&nbsp;';
+  }
+};
+
+map.on('pointermove', function (evt) {
+  if (evt.dragging) {
+    return;
+  }
+  displayFeatureInfo(evt.pixel);
+});
+
+map.on('click', function (evt) {
+  displayFeatureInfo(evt.pixel);
+});
